23db2275f3504e93c8a586a1b1f1e160e48823f6
kimkyoungmin97 <kmboy1210@gmail.com>
2025-04-07 14:43:35 +0900

    2025-04-07 first

---
+++ ahn/src/main/java/com/a5a5lab/AhnApplication.java
@@ -0,0 +1,13 @@
+package com.a5a5lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhnApplication.class, args);
+	}
+
+}
