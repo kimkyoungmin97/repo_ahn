1e09014f39d00b620010e721957be266fbb96dab
sung0615 <kk1hhh1fff1@naver.com>
2025-04-08 16:50:04 +0900

    코드 리스트 폼 업데이트 삭제

---
+++ ahn/src/main/java/com/a5a5lab/module/code/CodeController.java
@@ -0,0 +1,108 @@
+package com.a5a5lab.module.code;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.a5a5lab.common.util.UtilDateTiem;
+
+@Controller
+public class CodeController {
+	
+	@Autowired
+	CodeService codeService;
+	
+	
+	/*
+	 * // 코드 리스트 사용자 회원 가입에 뿌려주기
+	 * 
+	 * @RequestMapping(value ="/SignupUserForm") public String SignupUserForm(CodeVo
+	 * vo, Model model) { model.addAttribute("list", codeService.selectList(vo));
+	 * return "user/signup/SignupUserForm"; }
+	 */
+	 
+	 
+	
+	// 리스트 보여주고 페이지네이션 보여주기
+	@RequestMapping(value = "/code/codeXdmList")
+	public String codeXdmList(CodeVo vo, Model model) {
+		
+		vo.setParamsPaging(codeService.selectOneCount(vo));
+		model.addAttribute("list", codeService.selectList(vo));
+		model.addAttribute("vo",vo);
+		
+		// UtilDateTiem 함수 가져와서 사용 
+		
+		vo.setShDateStart(vo.getShDateStart() == null || vo.getShDateStart() == "" ? null : UtilDateTiem.add00TimeString(vo.getShDateStart()));
+		vo.setShDateEnd(vo.getShDateEnd() == null || vo.getShDateEnd() == "" ? null : UtilDateTiem.add59TimeString(vo.getShDateEnd()));
+		
+		return "xdm/code/codeXdmList";
+	}
+	
+	
+	// 등록폼
+	@RequestMapping(value="/code/CodeXdmForm")
+	public String CodeXdmForm( CodeDto codeDto, Model model , CodeVo vo) {
+		
+		
+		model.addAttribute("list", codeService.codeGroupNameSeq(codeDto));
+		model.addAttribute("vo", vo);
+		
+		
+		if (vo.getCdSeq().equals("0") || vo.getCdSeq().equals("")) {	
+			
+		} else {
+//			update mode			
+			model.addAttribute("item", codeService.selectOne(vo));
+			
+		}
+		
+		return "xdm/code/codeXdmForm";
+	}
+	
+	// 등록후 리스트 에서 보여주라
+	@RequestMapping(value="/code/CodeXdmInst")
+	public String CodeXdmInst(CodeDto codeDto) {
+		System.out.println(codeDto.getCodeGroup_cdgSeq());
+		codeService.insert(codeDto);
+		return "redirect:/code/codeXdmList";
+	}
+	
+	//업데이트
+	@RequestMapping(value="/code/CodeXdmUpdt")
+	public String CodeXdmUpdt(CodeDto codeDto) {
+		codeService.update(codeDto);
+		return "redirect:/code/codeXdmList";
+	}
+	
+	
+	
+	
+	// 업데이트 삭제
+	@RequestMapping(value="/code/CodeXdmUele")
+	public String CodeXdmUele(CodeDto codeDto) {
+		codeService.uelete(codeDto);
+		return "redirect:/code/codeXdmList";
+	}
+	
+	
+	/*
+	 * //데이터 1개씩 뽑아서 폼 화면 뿌리기
+	 * 
+	 * @RequestMapping(value="/code/CodeView") public String CodeView(Model
+	 * model,CodeDto codeDto,CodeVo vo) {
+	 * 
+	 * 
+	 * model.addAttribute("item", codeService.selectOne(codeDto,vo));
+	 * 
+	 * return"xdm/code/CodeXdmForm"; }
+	 */
+	
+		
+	
+	
+		
+		
+		
+}
---
+++ ahn/src/main/java/com/a5a5lab/module/code/CodeDto.java
@@ -0,0 +1,87 @@
+package com.a5a5lab.module.code;
+
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CodeDto {
+	
+	private String cdSeq; // 코드Seq
+	private String ifcdName; //코드 이름
+	private Integer ifcdDelNy; //업데이트 삭제
+	private String newsAgencyName; //통신사 이름
+	private String codeGroup_cdgSeq; //코드구릅Seq
+	private String ifcgName; // 코드 그룹 이름
+	private String cdgSeq;// 코드 그룹 Seq;
+//	-----
+//	for cache
+//	검색 날짜
+	private Date regDateTime;
+	private Date modDateTime;
+//	----
+	
+	
+	
+	public static List<CodeDto> cachedCodeArrayList = new ArrayList<CodeDto>();
+	
+	
+	public String getCdSeq() {
+		return cdSeq;
+	}
+	public void setCdSeq(String cdSeq) {
+		this.cdSeq = cdSeq;
+	}
+	public String getIfcdName() {
+		return ifcdName;
+	}
+	public void setIfcdName(String ifcdName) {
+		this.ifcdName = ifcdName;
+	}
+	public Integer getIfcdDelNy() {
+		return ifcdDelNy;
+	}
+	public void setIfcdDelNy(Integer ifcdDelNy) {
+		this.ifcdDelNy = ifcdDelNy;
+	}
+	public String getNewsAgencyName() {
+		return newsAgencyName;
+	}
+	public void setNewsAgencyName(String newsAgencyName) {
+		this.newsAgencyName = newsAgencyName;
+	}
+	public String getCodeGroup_cdgSeq() {
+		return codeGroup_cdgSeq;
+	}
+	public void setCodeGroup_cdgSeq(String codeGroup_cdgSeq) {
+		this.codeGroup_cdgSeq = codeGroup_cdgSeq;
+	}
+	
+
+//	----
+	public String getIfcgName() {
+		return ifcgName;
+	}
+	public void setIfcgName(String ifcgName) {
+		this.ifcgName = ifcgName;
+	}
+	public String getCdgSeq() {
+		return cdgSeq;
+	}
+	public void setCdgSeq(String cdgSeq) {
+		this.cdgSeq = cdgSeq;
+	}
+	public Date getRegDateTime() {
+		return regDateTime;
+	}
+	public void setRegDateTime(Date regDateTime) {
+		this.regDateTime = regDateTime;
+	}
+	public Date getModDateTime() {
+		return modDateTime;
+	}
+	public void setModDateTime(Date modDateTime) {
+		this.modDateTime = modDateTime;
+	}
+	
+	
+}
