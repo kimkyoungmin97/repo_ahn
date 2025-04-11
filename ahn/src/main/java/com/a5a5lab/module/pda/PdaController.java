c2d53e545a093b643f6add19420dbf4939883341
sung0615 <kk1hhh1fff1@naver.com>
2025-04-11 19:02:13 +0900

    ...

--- ahn/src/main/java/com/a5a5lab/module/pda/PdaController.java
+++ ahn/src/main/java/com/a5a5lab/module/pda/PdaController.java
@@ -1,20 +1,92 @@
 package com.a5a5lab.module.pda;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PdaController {
 	
-	// 입고하기
+	@Autowired
+	PdaService pdaService;
+	
+	
+	
+	//Pad 입고목록
 	@RequestMapping(value="/LnboundPad")
-	public String LnboundPad() {
+	public String LnboundPad(Model model) {
+		
+		model.addAttribute("list", pdaService.FactoryOrderList());
+		
+		
+		
 		return"pda/LnboundPad";
 	}
 	
-	//출고하기
+	//Pad 입고하기 상세
+	@RequestMapping("/LnboundPadDetailsPad")
+	public String LnboundPadDetailsPad(@RequestParam(value = "orderSeq", required = false) String orderSeqStr, Model model) {
+	    if (orderSeqStr != null && orderSeqStr.matches("\\d+")) {
+	        Integer orderSeq = Integer.valueOf(orderSeqStr);
+	        PdaDto order = pdaService.selectOne(orderSeq);
+	        if (order != null) {
+	            model.addAttribute("list", order);
+	            model.addAttribute("orderSeq", orderSeq);
+	        } else {
+	            model.addAttribute("errorMessage", "조회 결과가 없습니다.");
+	        }
+	    } else {
+	        model.addAttribute("errorMessage", "유효한 발주번호가 아닙니다.");
+	    }
+	    return "pda/LnboundPadDetailsPad";
+	}
+
+
+
+	// Pda 입고하기 출고대기에서 출고 요청으로 변경 업데이트
+	@RequestMapping(value="/LnboundPadUdate")
+	public String LnboundPadUdate(@RequestParam(value="orderSeq", required=false) Integer orderSeq) {
+	    if (orderSeq != null) {
+	      
+	    }
+	    pdaService.update(orderSeq);
+	    return "redirect:/pda/LnboundPadList";
+	}
+
+	
+	@ResponseBody
+	@RequestMapping("/api/getInboundOrder")
+	public Map<String, Object> getInboundOrder(@RequestParam("orderSeq") Integer orderSeq) {
+	    Map<String, Object> resultMap = new HashMap<>();
+	    
+	    System.out.println(orderSeq);
+	    
+//	    PdaDto dto = pdaService.selectOne(orderSeq);
+//	    if (dto != null) {
+//	        resultMap.put("rt", "success");
+//	        resultMap.put("order", dto);
+//	    } else {
+//	        resultMap.put("rt", "fail");
+//	    }
+
+	    return resultMap;
+	}
+	
+	
+	
+	
+	//Pad 배송하기
 	@RequestMapping(value="/DeliveryPda")
-	public String  DeliveryPda() {
+	public String  DeliveryPda(Model model) {
+		
+		model.addAttribute("list", pdaService.selectList());
+		
 		return "/pda/DeliveryPda";
 	}
 	
---
+++ ahn/src/main/java/com/a5a5lab/module/pda/PdaService.java
@@ -0,0 +1,54 @@
+package com.a5a5lab.module.pda;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.a5a5lab.module.order.OrderVo;
+
+@Service
+public class PdaService {
+	
+	
+	@Autowired
+	PdaDao pdaDao;
+	
+	
+	
+	
+	
+	// 발주 목록ㄹ스트 뿌리기
+	public List<PdaDto> FactoryOrderList(){
+		return pdaDao.FactoryOrderList();
+	}
+	//Pda 입고 (발주내역)을 출고대기에서 출고 완료로 업데이트 하기
+	public int update (Integer orderSeq) {
+		return pdaDao.update(orderSeq);
+	}
+	
+	//Pda 입고 발주 내역 데이터 1개씩 뽑기
+	public PdaDto selectOne(Integer orderSeq) {
+		return pdaDao.selectOne(orderSeq);
+	}
+
+
+	// 주문 배송 목록 리스트 뿌리기
+	public List<PdaDto> selectList(){
+		return pdaDao.selectList();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
